a635b3283089f1f37b43f5884160ef747bdef93d
Anish Ghosh <anishghosh103@gmail.com>
2018-12-23 22:02:04 +0530

    structure

--- src/app/modules/auth/auth.module.ts
+++ src/app/modules/auth/auth.module.ts
@@ -2,9 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './container/login/login.component';
 import { SharedModule } from '../shared/shared.module';
-import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
